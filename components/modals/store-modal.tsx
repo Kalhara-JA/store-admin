3c2551f0778e1b8417e850e02fafbb9d9cc7ad9c
Kalhara JA <kalharajak.21@uom.lk>
2023-11-13 13:44:11 +0530

    chore(component): update store-modal for sending api request

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,42 +1,57 @@
 'use client'
 
+import { useState } from 'react'
 import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 import { useStoreModal } from '@/hooks/use-store-modal'
-import { Modal } from '../ui/modal'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from '@/components/ui/form'
+import { Modal } from '@/components/ui/modal'
 import { Input } from '@/components/ui/input'
-import { Button } from '../ui/button'
+import { Button } from '@/components/ui/button'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form'
 
 const formSchema = z.object({
-  name: z.string().nonempty({ message: 'Store name is required' })
+    name: z.string().nonempty({ message: 'Store name is required' })
 })
 
 export const StoreModal = () => {
-  const storeModal = useStoreModal()
+    const storeModal = useStoreModal()
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: ''
+    const [loading, setLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: ''
+        }
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true)
+
+            const response = await axios.post('/api/stores', values)
+
+            toast.success("Store created.");
+
+        } catch(error) {
+            toast.error("Something went wrong");
+        }finally {
+            setLoading(false)
+        }
     }
-  })
 
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-    // TODO: Create store
-  }
-
-  return (
+    return (
         <Modal
             title='Create Store'
             description='Add a new store to manage products and categories'
@@ -55,28 +70,34 @@ export const StoreModal = () => {
                                         <FormLabel>Name</FormLabel>
                                         <FormControl>
                                             <Input
+                                                disabled={loading}
                                                 {...field}
                                                 type="text"
                                                 placeholder="Store Name"
                                                 className="input"
                                             />
                                         </FormControl>
-                                        <FormMessage/>
+                                        <FormMessage />
                                     </FormItem>
                                 )}
                             />
                             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
                                 <Button
+                                    disabled={loading}
                                     variant='outline'
                                     onClick={storeModal.onClose}>
                                     Cancel
                                 </Button>
-                                <Button type='submit'> Continue</Button>
+                                <Button
+                                    disabled={loading}
+                                    type='submit'>
+                                    Continue
+                                </Button>
                             </div>
                         </form>
                     </Form>
                 </div>
             </div>
         </Modal>
-  )
+    )
 }
